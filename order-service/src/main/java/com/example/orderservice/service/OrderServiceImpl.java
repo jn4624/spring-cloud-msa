5a1b365ce43864bcea2638f5dcd7d1fa401cba44
jn4624 <jn4624@gmail.com>
2024-11-15 11:13:32 +0900

    Order Service - 프로젝트 생성 및 기능 구현

---
+++ order-service/src/main/java/com/example/orderservice/service/OrderServiceImpl.java
@@ -0,0 +1,42 @@
+package com.example.orderservice.service;
+
+import com.example.orderservice.dto.OrderDto;
+import com.example.orderservice.entity.OrderEntity;
+import com.example.orderservice.jpa.OrderRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+    private final OrderRepository orderRepository;
+    private final ModelMapper modelMapper;
+
+    public OrderServiceImpl(OrderRepository orderRepository, ModelMapper modelMapper) {
+        this.orderRepository = orderRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public OrderDto createOrder(OrderDto orderDto) {
+        orderDto.setOrderId(UUID.randomUUID().toString());
+        orderDto.setTotalPrice(orderDto.getUnitPrice() * orderDto.getQty());
+
+        OrderEntity orderEntity = modelMapper.map(orderDto, OrderEntity.class);
+        orderRepository.save(orderEntity);
+
+        return modelMapper.map(orderEntity, OrderDto.class);
+    }
+
+    @Override
+    public OrderDto getOrderByOrderId(String orderId) {
+        OrderEntity orderEntity = orderRepository.findByOrderId(orderId);
+        return modelMapper.map(orderEntity, OrderDto.class);
+    }
+
+    @Override
+    public Iterable<OrderEntity> getOrdersByUserId(String userId) {
+        return orderRepository.findByUserId(userId);
+    }
+}
