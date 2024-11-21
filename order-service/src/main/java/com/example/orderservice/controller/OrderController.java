d52dc1166f719fd643a31d71a632be7f2d414df8
jn4624 <jn4624@gmail.com>
2024-11-21 20:46:36 +0900

    데이터 동기화 Apache Kafka - Order Service  Producer 설정 및 구현

--- order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.example.orderservice.controller;
 
 import com.example.orderservice.dto.OrderDto;
 import com.example.orderservice.entity.OrderEntity;
+import com.example.orderservice.messagequeue.KafkaProducer;
 import com.example.orderservice.service.OrderService;
 import com.example.orderservice.vo.RequestOrder;
 import com.example.orderservice.vo.ResponseOrder;
@@ -20,11 +21,14 @@ public class OrderController {
     private final Environment environment;
     private final OrderService orderService;
     private final ModelMapper modelMapper;
+    private final KafkaProducer kafkaProducer;
 
-    public OrderController(Environment environment, OrderService orderService, ModelMapper modelMapper) {
+    public OrderController(Environment environment, OrderService orderService,
+                           ModelMapper modelMapper, KafkaProducer kafkaProducer) {
         this.environment = environment;
         this.orderService = orderService;
         this.modelMapper = modelMapper;
+        this.kafkaProducer = kafkaProducer;
     }
 
     @GetMapping("/health_check")
@@ -35,11 +39,16 @@ public class OrderController {
     @PostMapping("/{userId}/orders")
     public ResponseEntity<ResponseOrder> createOrder(@PathVariable(name = "userId") String userId,
                                                      @RequestBody RequestOrder requestOrder) {
+        // jpa
         OrderDto orderDto = modelMapper.map(requestOrder, OrderDto.class);
         orderDto.setUserId(userId);
         OrderDto createdOrder = orderService.createOrder(orderDto);
 
         ResponseOrder result = modelMapper.map(createdOrder, ResponseOrder.class);
+
+        // send this order to the kafka
+        kafkaProducer.send("example-catalog-topic", orderDto);
+
         return ResponseEntity.status(HttpStatus.CREATED).body(result);
     }
 
